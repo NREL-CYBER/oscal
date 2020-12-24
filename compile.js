2abe3e15f13cb8171d0eed47bfc8a29daca7f042
Paul Wand <Paul.Wand@nrel.gov>
2020-12-24 15:47:03 -0700

    barrel types

--- compile.js
+++ compile.js
@@ -3,33 +3,34 @@ fs = require("fs");
 
 
 console.log("Initiating schema Compilation")
-schemaCompiler.compileFromFile('./schemas/oscal_assessment-plan_schema.json')
+const options = {}
+schemaCompiler.compileFromFile('./schemas/oscal_assessment-plan_schema.json', options)
     .then(ts => fs.writeFileSync('./assessment-plan/index.d.ts', ts))
     .then(() => console.log("✔️  Assessment Plan"))
 
 
-schemaCompiler.compileFromFile('./schemas/oscal_assessment-results_schema.json')
+schemaCompiler.compileFromFile('./schemas/oscal_assessment-results_schema.json', options)
     .then(ts => fs.writeFileSync('./assessment-results/index.d.ts', ts))
     .then(() => console.log("✔️  Assessment Results"))
 
-schemaCompiler.compileFromFile('./schemas/oscal_catalog_schema.json')
+schemaCompiler.compileFromFile('./schemas/oscal_catalog_schema.json', options)
     .then(ts => fs.writeFileSync('./types/catalog/index.d.ts', ts))
     .then(() => console.log("✔️  Catalog"))
 
-schemaCompiler.compileFromFile('./schemas/oscal_component_schema.json')
+schemaCompiler.compileFromFile('./schemas/oscal_component_schema.json', options)
     .then(ts => fs.writeFileSync('./component/index.d.ts', ts))
     .catch(console.log)
     .then(() => console.log("✔️  Component"))
 
-schemaCompiler.compileFromFile('./schemas/oscal_profile_schema.json')
+schemaCompiler.compileFromFile('./schemas/oscal_profile_schema.json', options)
     .then(ts => fs.writeFileSync('./baseline-profile/index.d.ts', ts))
     .then(() => console.log("✔️  Baseline Profile"))
 
-schemaCompiler.compileFromFile('./schemas/oscal_poam_schema.json')
+schemaCompiler.compileFromFile('./schemas/oscal_poam_schema.json', options)
     .then(ts => fs.writeFileSync('./plan-of-action-and-milestones/index.d.ts', ts))
     .then(() => console.log("✔️  Plan of Action & Milestones"))
 
-schemaCompiler.compileFromFile('./schemas/oscal_ssp_schema.json')
+schemaCompiler.compileFromFile('./schemas/oscal_ssp_schema.json', options)
     .then(ts => fs.writeFileSync('./system-security-plan/index.d.ts', ts))
     .then(() => console.log("✔️  System Security Plan"))
 
--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,15 @@
-export interface Oscal {
-    profile?: Profile;
+export * from './baseline-profile'; // re-export all of its exports
+export * from "./catalog"
+export * from "./system-security-plan"
+export * from "./assessment-results"
+export * from "./assessment-plan"
+export * from "./plan-of-action-and-milestones"
+
+export default interface Oscal {
+    profile: Profile,
     catalog?: Catalog;
     systemSecurityPlan?: SystemSecurityPlanSSP;
-    assessmentPlan?: SecurityAssessmentPlanSAP;
-    assessmentResults?: SecurityAssessmentResultsSAR;
+    assessmentPlan?: SecurityAssessmentResultsSAR;
+    assessmentResults?: SecurityAssessmentPlanSAP;
     planOfActionAndMilestones?: PlanOfActionAndMilestonesPOAM;
 }
