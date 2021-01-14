73454b60734783c272e2b9c971f751c932ec26d3
Paul Wand <Paul.Wand@nrel.gov>
2021-01-14 15:46:36 -0700

    new up regex pattern

--- dist/importOscal.js
+++ dist/importOscal.js
@@ -12,7 +12,7 @@ var importOscal = function importOscal(input) {
   // We do convert roles like authorizing-offical to authorizing_official
   return snakeCaseKeys(input, {
     deep: true,
-    exclude: /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+    exclude: new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
   });
 };
 
