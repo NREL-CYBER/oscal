184fc9835c99d53466bd1a1ebe442a44a0359acd
Paul Wand <Paul.Wand@nrel.gov>
2021-01-27 10:19:07 -0700

    update oscal content

--- tests/validate.ts
+++
@@ -1,6 +0,0 @@
-import ssp from "../schemas/oscal_ssp_schema.json"
-import Ajv, { ValidateFunction } from "ajv"
-import addFormats from "ajv-formats"
-const jsonValidator = new Ajv();
-addFormats(jsonValidator)
-jsonValidator.addSchema(ssp);
