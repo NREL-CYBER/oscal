796a7e4f31350de80bdc889692b25ba9b7598970
Paul Wand <Paul.Wand@nrel.gov>
2021-05-19 15:10:53 -0600

    add accepeted types

---
+++ src/accepted_values/index.ts
@@ -0,0 +1,11 @@
+import fedramp_extensions from "../../content/accepted-value/fedramp-extensions.json"
+
+export const queryExtensions = (binding: string, ns: string) => {
+    if (ns === "https://fedramp.gov/ns/oscal") {
+        return Object
+            .entries(fedramp_extensions.extensions.extensions)
+            .filter(([_, extension_data]) =>
+                extension_data.bindings.map(x => x.pattern).includes(binding)
+            );
+    }
+}
